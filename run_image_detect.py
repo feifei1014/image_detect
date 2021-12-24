7561b7dfc13a6eaa8610dd50ad325ed4117a1a9f
Feifei <53549285+feifei1014@users.noreply.github.com>
2021-12-25 00:58:31 +0800

    Add files via upload

--- run_image_detect.py
+++ run_image_detect.py
@@ -3,5 +3,5 @@
 import sys
 from image_detect_src.image_detect_src import cuiqifan
 
-cuiqifan( data_path=sys.argv[1] , res_path=sys.argv[2] , gain=sys.argv[3] ,
-          magzp=sys.argv[4] , seeingdetimage=sys.argv[5])
+cuiqifan( data_path=sys.argv[1] , res_path=sys.argv[2] ,detimage=sys.argv[3])
+
