4c80c7ef54cae34a1bf3421315487ac626e37f3e
Feifei <53549285+feifei1014@users.noreply.github.com>
2021-12-25 00:50:46 +0800

    update1

--- run_image_detect.py
+++ run_image_detect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.8
 
 import sys
-from image_detect_src import cuiqifan
+from image_detect_src.image_detect_src import cuiqifan
 
 cuiqifan( data_path=sys.argv[1] , res_path=sys.argv[2] , gain=sys.argv[3] ,
           magzp=sys.argv[4] , seeingdetimage=sys.argv[5])
